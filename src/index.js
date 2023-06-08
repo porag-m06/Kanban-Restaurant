df00dee94aa5881a61acb6f16cefabf364387d5c
porag-m06 <porag.m06@gmail.com>
2023-06-08 20:24:20 +0600

    eventlistener fixed to reservation buttons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-/** @format */
-
 import "./style.css";
 import getAllSeaFoodMeals from "./meals.js";
 import logo from "../assets/logo-kan.png";
-import getItem from "./modules/reservation.js";
-
+import getItem from "./modules/reservation2";
 
 const logoImg = document.querySelector(".logo");
 logoImg.src = logo;
@@ -31,10 +28,11 @@ const showCards = (foodArray) => {
                <h6>ID: ${foodArray[index].idMeal}</h6>
                <img src="${foodArray[index].strMealThumb}" alt="" >
                <button class="comment">Comments</button>
-               <button class="reserve">Reservations</button>
+               <button class="reserve reserve${index}">Reservations</button>
                `;
 		container.appendChild(cardDiv);
-		setEventToMealId(foodArray[index].idMeal);
+		const btnReserve = document.querySelector(`.reserve${index}`); 
+		setEventForReservation(btnReserve,foodArray[index].idMeal);
 	}
 };
 
@@ -46,17 +44,14 @@ setTimeout(() => {
 		console.log("No data found while fetching for meal list!!!");
 		/* eslint-enable */
 	}
-}, 1000);
+}, 2000);
 
-const mealId = 52772;
-
-const setEventToMealId = (mealId) => {
-	document.addEventListener("click", (e) => {
+const setEventForReservation = (btnReserve,mealId) => {
+	btnReserve.addEventListener("click", (e) => {
+		console.log("R Clicked");
 		if (e.target.classList.contains("reserve")) {
 			// modal.style.display = "block";
 			getItem(mealId);
 		}
 	});
 };
-
-
